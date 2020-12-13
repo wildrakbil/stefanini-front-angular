c9abd92dcc1097b2baebd656faf1c1b7936ddf2b
Oscar Calero <ocalero@processoft.com.co>
2020-12-13 17:55:57 -0500

    ocalero

--- src/properties/properties.lps.ts
+++ src/properties/properties.lps.ts
@@ -4,41 +4,42 @@ Propiedades variabilidad de la LPS
 */
 import { InjectionToken } from '@angular/core';
 export const propertiesLps = {
-  general:{
-    nombre    : "Taller Virtual",
-    direccion : "Calle 150 # 999",
-    telefono  : "5551122",
-    paginaweb : "www.tallervirtual.com",
-    logo      : "https://img.freepik.com/vector-gratis/plantilla-logo-taller_1043-187.jpg?size=338&ext=jpg"
+  general: {
+    nombre: 'Taller Virtual',
+    direccion: 'Calle 150 # 999',
+    telefono: '5551122',
+    paginaweb: 'www.tallervirtual.com',
+    logo:
+      'https://img.freepik.com/vector-gratis/plantilla-logo-taller_1043-187.jpg?size=338&ext=jpg',
   },
-  componentes:{
-    footer:true,
-    reservar:true,
-    servicios:{
-      lista:true,
-      mosaico:true,
-      galeria:true
-    }
-  }
+  componentes: {
+    footer: true,
+    reservar: true,
+    servicios: {
+      lista: true,
+      mosaico: true,
+      galeria: true,
+    },
+  },
 };
 
 export interface IPropertiesLPS {
-  general:{
-    nombre:string,
-    direccion:string,
-    telefono:string,
-    paginaweb:string,
-    logo:string
-  },
-  componentes:{
-    footer:boolean,
-    reservar:boolean,
-    servicios:{
-      lista:boolean,
-      mosaico:boolean,
-      galeria:boolean
-    }
-  }
+  general: {
+    nombre: string;
+    direccion: string;
+    telefono: string;
+    paginaweb: string;
+    logo: string;
+  };
+  componentes: {
+    footer: boolean;
+    reservar: boolean;
+    servicios: {
+      lista: boolean;
+      mosaico: boolean;
+      galeria: boolean;
+    };
+  };
 }
 
 export const APP_CONFIG = new InjectionToken('Application config');
